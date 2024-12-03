3ee6d396cdb8db7d1d094c2c8030aac44139235e
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-03 15:34:34 +0300

    Add files via upload

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalImages = 52;
+            int lengthOfImages = 3;
+            int columnsOfImages;
+            int restOfImages;
+
+            columnsOfImages = totalImages / lengthOfImages;
+            restOfImages = totalImages % lengthOfImages;
+
+            Console.WriteLine($"Полностью заполненных рядов можно будет вывести - {columnsOfImages}");
+            Console.WriteLine($"Картинок будет сверх меры - {restOfImages}");
+    }         
+    }
+}
